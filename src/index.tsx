537972024b67045c743a09a7a670a9eaacd0b39d
AntonioIliev19 <ilievantonio19@gmail.com>
2025-03-29 11:02:42 +0200

    Convert to Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const container = document.getElementById("root");
+
+if (!container) throw new Error("Could not find container");
+
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
