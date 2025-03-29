5a26c976f20449058d05cbf62dd3a4490ef44d8c
AntonioIliev19 <ilievantonio19@gmail.com>
2025-03-29 14:10:41 +0200

    Edit default page to redirect to register.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -9,6 +9,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to={"/register"} replace />} />
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/dashboard" element={
